f6fcee6311e5fed3154f16a2d6612c432753ade5
j-jwolf <jjwolf@noctrl.edu>
2020-10-25 17:07:24 -0500

    finished implementing methods for file reading

--- src/hw4.cpp
+++ src/hw4.cpp
@@ -44,7 +44,9 @@ using namespace std;
  * 	- Are we able to use getline(cin, var) to read input or should we use while(!sentinel) {cin >> var;} instead?
  * To do:
  * 	- Finish adding methods to date from your laptop. Just manually add them, there's only a few short methods
- * 	-
+ * 	- You aren't reading the data in correctly. You need to check if is contacted:
+ * 	  -- if yes, there is a date and doctor id
+ * 	  -- else, no date or doctor id
  * Notes:
  * 	- What to code first:
  * 		- Finish adding the accessers/mutators first. Those are easy and will really shape what to do next on their own
@@ -100,9 +102,12 @@ public:
 	}
 	Date(string s)
 	{
-		cout << "in string type conversion function" << endl;
+		cout << "Date n-arg constructor" << endl;
+		cout << "string arg: " << s << endl;
 		convert(s);
+		cout << "date: " << getDate() << endl;
 	}
+	string getDate() {return convert2();}
 	const Date& operator++()
 	{
 		cout << "in prefix operator++" << endl;
@@ -180,7 +185,13 @@ private:
 	string _email;
 public:
 	Person();
-	Person(int p, string fn, string ln, string em);
+	Person(int p, string fn, string ln, string em)
+	{
+		_pid = p;
+		_fname = fn;
+		_lname = ln;
+		_email = em;
+	}
 	void setPid(int p) {_pid = p;}
 	void setFname(string fn) {_fname = fn;}
 	void setLname(string ln) {_lname = ln;}
@@ -189,6 +200,13 @@ public:
 	const string getFname() {return _fname;}
 	const string getLname() {return _lname;}
 	const string getEmail() {return _email;}
+	void testPrint()
+	{
+		cout << "=================================================" << endl;
+		cout << "PID: " << _pid << endl;
+		cout << "First name: " << _fname << ", Last name: " << _lname << endl;
+		cout << "Email: " << _email << endl;
+	}
 };
 class Case
 {
@@ -207,14 +225,32 @@ private:
 public:
 	// need set/get cid, pid, diag, whoconf, casestatus
 	Case(){}
-	Case(int cid, int pid, Date date, int diag, int whoconf, int contacted)
+	Case(int cid, int pid, string date, int diag, int whoconf, int contacted)
 	{
 		_cid = cid;
 		_pid = pid;
-		_diagDate = date;
+		if(date != "") {Date d(date); _diagDate = d;}
 		_diagnosis = diag;
 		_whoConf = whoconf;
 		_contacted = contacted;
+		_caseStatus = 0;
+	}
+	Case(int cid, int pid, int diag, int whoconf, int contacted, int status)
+	{
+		_cid = cid;
+		_pid = pid;
+		_diagnosis = diag;
+		_whoConf = whoconf;
+		_contacted = contacted;
+		_caseStatus = status;
+	}
+	Case(int cid, int pid)
+	{
+		_cid = cid;
+		_pid = pid;
+		_contacted = 0;
+		_whoConf = -1;
+		_diagnosis = -1;
 		_caseStatus = 0;
 	}
 	void setContacted(int contacted) {_contacted = contacted;}
@@ -223,12 +259,14 @@ public:
 	void setDiag(int diagnosis) {_diagnosis = diagnosis;}
 	void setWhoConfirmed(int docId) {_whoConf = docId;}
 	void setCaseStatus(int caseStatus) {_caseStatus = caseStatus;}
+	void setDiagDate(string date) {Date d(date); _diagDate = d;}
 	int getContacted() {return _contacted;}
 	int getCid() {return _cid;}
 	int getPid() {return _pid;}
 	int getDiag() {return _diagnosis;}
 	int getWhoConfirmed() {return _whoConf;}
 	int getCaseStatus() {return _caseStatus;}
+	string getDiagDate() {return _diagDate.getDate();}
 	/*
 	 * pre: parameter must be 0 to length-1 of vector of symptoms
 	 * post: symptom returned or empty string if parameter out of bounds
@@ -263,6 +301,67 @@ public:
 		if(index < 0 || index > _severity.size()) {return;}
 		std::vector<int>::iterator vit = _severity.begin()+index;
 		_severity.erase(vit);
+	}
+	int getSeverity(int index) {return _severity[index];}
+	int getOrigPid(int index) {return _original[index];}
+	void addOrigPid(int pid) {_original.push_back(pid);}
+	void removeOrigPid(int index)
+	{
+		if(index < 0 || index > _original.size()) {return;}
+		std::vector<int>::iterator vit = _original.begin()+index;
+		_severity.erase(vit);
+	}
+	int getAssociatedPid(int index) {return _associated[index];}
+	void addAssociatedPid(int pid) {_associated.push_back(pid);}
+	void removeAssociatedPid(int index)
+	{
+		if(index < 0 || index > _associated.size()) {return;}
+		std::vector<int>::iterator vit = _associated.begin()+index;
+		_associated.erase(vit);
+	}
+	int originalCount() {return _original.size();}
+	int associatedCount() {return _associated.size();}
+	void testPrint()
+	{
+		cout << "=================================================" << endl;
+		cout << "Case ID: " << _cid << endl;
+		cout << "Person ID: " << _pid << endl;
+		cout << "Diagnosis: ";
+		if(_diagnosis == 1) {cout << "Positive";}
+		else if(_diagnosis == 0) {cout << "Negative";}
+		else {cout << "No diagnosis information";}
+		cout << endl << "Doctor ID: ";
+		if(_whoConf != -1) {cout << _whoConf << endl;}
+		else {cout << "no doctor confirmation" << endl;}
+		cout << "Contacted?: ";
+		if(_contacted) {cout << "Yes";}
+		else {cout << "No";}
+		cout << endl << "Patient symptoms" << endl;
+		int length = _symptoms.size();
+		for(int i = 0; i < length; i++)
+		{
+			cout << "Symptom: " << _symptoms[i] << ", is confirmed?: ";
+			if(_confirmed[i]) {cout << "Yes";}
+			else {cout << "No";}
+			cout << ", severity: " << _severity[i] << endl;
+		}
+		int oLen = _original.size(), aLen = _associated.size();
+		if(oLen > 0)
+		{
+			cout << "Original case IDs: " << _original[0];
+			for(int i = 1; i < oLen; i++) {cout << ", " << _original[i];}
+			cout << endl;
+		}
+		if(aLen > 0)
+		{
+			cout << "Associated case IDs: " << _associated[0];
+			for(int i = 1; i < aLen; i++) {cout << ", " << _associated[i];}
+			cout << endl;
+		}
+		cout << "Is case closed?: ";
+		if(_caseStatus) {cout << "Yes";}
+		else {cout << "No";}
+		cout << endl;
 	}
 };
 class AllPersons
@@ -322,9 +421,16 @@ public:
 	}
 	void addPerson(int pid, string fname, string lname, string email)
 	{
-		std::vector<Person*>::iterator vit = _persons.begin();
-		while(vit != _persons.end() && (*vit)->getPid() != pid) {vit++;}
-		if(vit != _persons.end()) {_persons.push_back(new Person(pid, fname, lname, email));}
+		if(_persons.size() == 0)
+		{
+			_persons.push_back(new Person(pid, fname, lname, email));
+		}
+		else
+		{
+			std::vector<Person*>::iterator vit = _persons.begin();
+			while(vit != _persons.end() && (*vit)->getPid() != pid) {vit++;}
+			if(vit == _persons.end()) {_persons.push_back(new Person(pid, fname, lname, email));}
+		}
 	}
 	void removePerson(int pid)
 	{
@@ -350,6 +456,16 @@ public:
 		while(vit != _persons.end() && (*vit)->getPid() != pid) {vit++;}
 		if(vit != _persons.end()) {(*vit)->setLname(lname);}
 	}
+	void testPrint()
+	{
+		int length = _persons.size();
+		cout << "person cnt: " << length << endl;
+		cout << "=================================================" << endl;
+		for(int i = 0; i < length; i++)
+		{
+			_persons[i]->testPrint();
+		}
+	}
 };
 
 class AllCases
@@ -367,11 +483,44 @@ public:
 		}
 	}
 	// can make a case here and modify everything else
-	void addCase(int cid, int pid, string fname, string lname, string email)
+	void addCase(int cid, int pid, string date, int diag, int whoconf, int contacted)
 	{
-		std::vector<Case*>::iterator vit = _cases.begin();
-		while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
-		if(vit != _cases.end()) {_cases.push_back(new Case());}
+		if(_cases.size() == 0)
+		{
+			_cases.push_back(new Case(cid, pid, date, diag, whoconf, contacted));
+		}
+		else
+		{
+			std::vector<Case*>::iterator vit = _cases.begin();
+			while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
+			if(vit == _cases.end()) {_cases.push_back(new Case(cid, pid, date, diag, whoconf, contacted));}
+		}
+	}
+	void addCase(int cid, int pid, int diag, int whoconf, int contacted, int status)
+	{
+		if(_cases.size() == 0)
+		{
+			_cases.push_back(new Case(cid, pid, diag, whoconf, contacted, status));
+		}
+		else
+		{
+			std::vector<Case*>::iterator vit = _cases.begin();
+			while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
+			if(vit == _cases.end()) {_cases.push_back(new Case(cid, pid, diag, whoconf, contacted, status));}
+		}
+	}
+	void addCase(int cid, int pid)
+	{
+		if(_cases.size() == 0)
+		{
+			_cases.push_back(new Case(cid, pid));
+		}
+		else
+		{
+			std::vector<Case*>::iterator vit = _cases.begin();
+			while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
+			if(vit == _cases.end()) {_cases.push_back(new Case(cid, pid));}
+		}
 	}
 	void addToCaseContacted(int cid, int contacted)
 	{
@@ -379,17 +528,28 @@ public:
 		while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
 		if(vit != _cases.end()) {(*vit)->setContacted(contacted);}
 	}
-	void addDiagnosisInfo(int cid, int diagnosis, int whoDiagnosed)
+	void addDiagnosisInfo(int cid, int diagnosis, int whoDiagnosed, string date)
 	{
 		std::vector<Case*>::iterator vit = _cases.begin();
 		while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
-		if(vit != _cases.end()) {(*vit)->setWhoConfirmed(whoDiagnosed); (*vit)->setDiag(diagnosis);}
+		if(vit != _cases.end())
+		{
+			(*vit)->setWhoConfirmed(whoDiagnosed);
+			(*vit)->setDiag(diagnosis);
+			(*vit)->setDiagDate(date);
+		}
 	}
 	void addToCaseSymptom(int cid, string symptom, int confirmed, int severity)
 	{
 		std::vector<Case*>::iterator vit = _cases.begin();
 		while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
 		if(vit != _cases.end()) {(*vit)->addSymptom(symptom); (*vit)->addConfirmed(confirmed); (*vit)->addSeverity(severity);}
+	}
+	void setCaseStatus(int cid, int status)
+	{
+		std::vector<Case*>::iterator vit = _cases.begin();
+		while(vit != _cases.end() && cid != (*vit)->getCaseStatus()) {vit++;}
+		if(vit != _cases.end()) {(*vit)->setCaseStatus(status);}
 	}
 	/*
 	 * More methods to write
@@ -398,10 +558,25 @@ public:
 	// !!! FOR TESTING, DELETE ME !!!
 	void testPrint()
 	{
-		for(int i = 0; i < _cases.size(); i++)
+		int length = _cases.size();
+		cout << "Length: " << length << endl;
+		cout << "=================================================" << endl;
+		for(int i = 0; i < length; i++)
 		{
-			int cid = _cases[i]->getCid(), pid = _cases[i]->getPid(), contacted = _cases[i]->getContacted();
+			_cases[i]->testPrint();
 		}
+	}
+	void addToCaseOriginal(int cid, int pid)
+	{
+		std::vector<Case*>::iterator vit = _cases.begin();
+		while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
+		if(vit != _cases.end()) {(*vit)->addOrigPid(pid);}
+	}
+	void addToCaseAssociated(int cid, int pid)
+	{
+		std::vector<Case*>::iterator vit = _cases.begin();
+		while(vit != _cases.end() && (*vit)->getCid() != cid) {vit++;}
+		if(vit != _cases.end()) {(*vit)->addAssociatedPid(pid);}
 	}
 };
 class AllSymptoms
@@ -444,38 +619,191 @@ vector<string> readFile(istream& infile)
 	cout << "line: " << line << endl;
 	while(!infile.eof())
 	{
-		//cout << "Debug" << endl << "line: " << line << endl;
+		cout << "Debug" << endl << "line: " << line << endl;
 		lines.push_back(line);
 		getline(infile, line);
 	}
+	cout << "Debug" << endl << "line: " << line << endl;
 	return lines;
 }
-vector<string> splitLine(string line)
+vector<string> splitLine(const string& line)
 {
 	vector<string> splitLines;
-	int count = 0, length = line.length();
-	stringstream ss;
-	while(count < length)
+	stringstream ss(line);
+	string s;
+	cout << "In splitLine: ";
+	while(getline(ss, s, ','))
 	{
-		char c = line[count];
-		if(c != ',') {ss << c;}
-		else
-		{
-			string t;
-			ss >> t;
-			splitLines.push_back(t);
-		}
-		count++;
+		cout << "s: " << s << " ";
+		splitLines.push_back(s);
 	}
+	cout << endl;
 	return splitLines;
 }
-void createCases(AllCases& cases, vector<string> data)
+string dequote(string& s)
 {
-	int length = data.size();
+	string temp;
+	stringstream ss(s);
+	vector<string> vec;
+	char lim = 34;
+	while(getline(ss, temp, lim))
+	{
+		cout << "in dequote, temp: " << temp << endl;
+		if(temp != " " && temp != "") {vec.push_back(temp);}
+	}
+	if(vec.size() > 0) {temp = vec[0];}
+	else {temp = "";}
+	return temp;
+}
+string removeSpaces(string& s)
+{
+	string temp;
+	stringstream ss(s);
+	vector<string> vec;
+	char lim = ' ';
+	while(getline(ss, temp, lim))
+	{
+		cout << "temp in remove spaces: " << temp << endl;
+		if(temp != " " && temp != "") {vec.push_back(temp);}
+	}
+	if(vec.size() > 0) {temp = vec[0];}
+	else {temp = "";}
+	cout << "temp to return: " << temp << endl;
+	return temp;
+}
+void addCase(AllCases& cases, AllPersons& persons, string data)
+{
+	// needs to split the line and then find out how you need to read the data in each line
+	// !!! DELETE ALL OF THE TEST PRINTS FROM HERE FOR FINAL VERSION !!!
+	vector<string> split = splitLine(data);
+	int length = split.size();
+	cout << "split length: " << length << endl;
+	cout << "test print data in addCase: " << data << endl;
+	cout << "test print split data in addCase" << endl << "split[0], split[1], split[2]: " << split[0] << ", " << split[1] << ", " << split[2] << endl;
 	for(int i = 0; i < length; i++)
 	{
-		//
+		cout << "split " << i << ": " << split[i] << endl;
 	}
+	int cid = stoi(split[0]);
+	cout << "cid made: " << cid << endl;
+	int pid = stoi(split[1]);
+	cout << "pid made: " << pid << endl;
+	string fname = split[2];
+	cout << "fname made: " << fname << endl;
+	string lname = split[3];
+	cout << "lname made: " << lname << endl;
+	string email = split[4];
+	cout << "email made: " << email << endl;
+	int contacted = stoi(split[5]);
+	cout << "contacted made: " << contacted << endl;
+	int symCnt = stoi(split[6]);
+	cout << "symCnt made: " << symCnt << endl;
+	vector<string> symList;
+	vector<int> confList, sevList;
+	int index = 7, lim = index+symCnt;
+	//index++;
+	cout << "index: " << index << ", lim: " << lim << endl;
+	while(index < lim)
+	{
+		string t1 = split[index];
+		int t2 = stoi(split[index+symCnt]), t3 = stoi(split[index+(symCnt*2)]);
+		symList.push_back(t1);
+		confList.push_back(t2);
+		sevList.push_back(t3);
+		index++;
+	}
+	index = index+(symCnt*2);
+	cout << "index after adding symptoms: " << index << endl;
+	// test here
+	cout << "cid: " << cid << endl << "pid: " << pid << endl << "fname: " << fname << endl << "lname: " << lname << endl << "email: " << email << endl;
+	cout << "contacted: " << contacted << endl << "symCnt: " << symCnt << endl;
+	for(int i = 0; i < symCnt; i++)
+	{
+		cout << "sym: " << symList[i] << ", confirmed: " << confList[i] << ", severity: " << sevList[i] << endl;
+	}
+	cout << "ending index: " << index << endl;
+	string diagDate = removeSpaces(split[index++]);
+	cout << "date: " << diagDate << endl << "index: " << index << endl;
+	string deqDate = "";
+	if(diagDate != "") {deqDate = dequote(diagDate);}
+	cout << "dequoted date: " << deqDate << endl;
+	int diagnosis = stoi(split[index++]);
+	int docid;
+	if(diagnosis != -1) {docid = stoi(split[index++]);}
+	else {docid = -1;}
+	cout << "diagnosis: " << diagnosis << endl << "docid: " << docid << endl;
+	int origCaseCnt = stoi(split[index++]);
+	lim = index+origCaseCnt;
+	cout << "origCaseCnt: " << origCaseCnt << endl << "index: " << index << endl << "lim: " << lim << endl;
+	vector<int> origCases, assocCases;
+	if(index < lim)
+	{
+		while(index < lim)
+		{
+			int origCid = stoi(split[index]);
+			origCases.push_back(origCid);
+			cout << "ocid " << index << ": " << origCid << endl;
+			index++;
+		}
+	}
+	int assocCaseCnt = stoi(split[index++]);
+	lim = index+assocCaseCnt;
+	cout << "assocCaseCnt: " << assocCaseCnt << endl << "index: " << index << endl << "lim: " << lim << endl;
+	if(index < lim)
+	{
+		while(index < lim)
+		{
+			int assocCid = stoi(split[index]);
+			assocCases.push_back(assocCid);
+			cout << "acid: " << index << ": " << assocCid << endl;
+			cout << assocCases[0] << endl;
+			index++;
+		}
+	}
+	int status = stoi(split[index++]);
+	cout << "status (closed?): " << status << endl;
+	if(contacted == 0)
+	{
+		cases.addCase(cid, pid);
+	}
+	else
+	{
+		cases.addCase(cid, pid, deqDate, diagnosis, docid, contacted);
+		cases.addToCaseContacted(cid, contacted);
+		cases.addDiagnosisInfo(cid, diagnosis, docid, deqDate);
+	}
+	/*cases.addCase(cid, pid, deqDate, diagnosis, docid, contacted);
+	cases.addToCaseContacted(cid, contacted);
+	cases.addDiagnosisInfo(cid, diagnosis, docid, deqDate);*/
+	if(symCnt > 0)
+	{
+		for(int i = 0; i < symCnt; i++)
+		{
+			string sym = symList[i];
+			int conf = confList[i];
+			int sev = sevList[i];
+			cases.addToCaseSymptom(cid, sym, conf, sev);
+		}
+	}
+	if(origCaseCnt > 0)
+	{
+		for(int i = 0; i < origCaseCnt; i++)
+		{
+			int origPid = origCases[i];
+			cout << "origPid" << origPid << endl;
+			cases.addToCaseOriginal(cid, origPid);
+		}
+	}
+	if(assocCaseCnt > 0)
+	{
+		for(int i = 0; i < assocCaseCnt; i++)
+		{
+			int assocPid = assocCases[i];
+			cout << "assocPid: " << assocPid << endl;
+			cases.addToCaseAssociated(cid, assocPid);
+		}
+	}
+	persons.addPerson(pid, fname, lname, email);
 }
 int main()
 {
@@ -504,5 +832,14 @@ int main()
 	cout << "Symptom list vector test" << endl;
 	for(int i = 0; i < symptomCount; i++) {cout << symptomList[i] << endl;}*/ //!!! DELETE ME, THIS IS FOR TESTING !!!
 	cout << "compiled" << endl;
+	//addCase(cases, persons, caseInfo[0]);
+	int caseCnt = caseInfo.size();
+	for(int i = 0; i < caseCnt; i++)
+	{
+		addCase(cases, persons, caseInfo[i]);
+		cout << "case " << i+1 << " was added" << endl;
+	}
+	cases.testPrint();
+	persons.testPrint();
 	return 0;
 }
